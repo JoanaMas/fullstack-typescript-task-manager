ea8ea72bef1270442056e827af90ca13ffad259a
JoanaMas <joana.mastianica@gmail.com>
2023-05-13 12:03:57 +0300

    3: Prop types integrated in profile component

--- client/react-task-manager/src/components/profile/Profile.tsx
+++ client/react-task-manager/src/components/profile/Profile.tsx
@@ -1,14 +1,18 @@
 import React, { FC, ReactElement } from 'react';
+import PropTypes from 'prop-types';
 // MUI components
 import { Avatar, Box, Typography } from '@mui/material';
 // Styles
 import * as Styled from './style';
 
-const Profile: FC = (): ReactElement => {
+const Profile: FC = (props: any): ReactElement => {
+
+  const { name = "Joana" } = props;
+
   return (
     <Box sx={Styled.profileContainer}>
-      <Avatar sx={Styled.avatar}>J</Avatar>
-      <Typography variant='h5' sx={{ color: "common.white" }}>Welcome, Joana</Typography>
+      <Avatar sx={Styled.avatar}>{`${name[0].toUpperCase()}`}</Avatar>
+      <Typography variant='h5' sx={{ color: "common.white" }}>Welcome, {name}</Typography>
       <Typography variant='body1'>This is your personal task manager</Typography>
       <Typography variant='body2' sx={{ opacity: '0.3', mt: "20px" }}>Enter required information in the form to create a task</Typography>
     </Box>
@@ -16,3 +20,10 @@ const Profile: FC = (): ReactElement => {
 };
 
 export default Profile;
+
+
+// USING PROP TYPES 
+
+Profile.propTypes = {
+  name: PropTypes.string,
+};
