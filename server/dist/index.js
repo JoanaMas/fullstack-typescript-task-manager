e39ff1311b20763af2637a58013a5cd71faf9368
JoanaMas <joana.mastianica@gmail.com>
2023-05-17 11:16:31 +0300

    13: Server connected to mySql database

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,15 +3,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppDataSource = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const typeorm_1 = require("typeorm");
 const app = (0, express_1.default)();
 dotenv_1.default.config();
-// Server port
+// Server port + port fetched from env file
 const port = process.env.PORT;
+// Database connection
+exports.AppDataSource = new typeorm_1.DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    synchronize: true,
+});
 // Default route
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
-// Listen to server
-app.listen(port);
+exports.AppDataSource.initialize().then(() => {
+    app.listen(port);
+    console.log('Data Source has benn initialized');
+}).catch((err) => {
+    console.error("Error during Data Source initialization", err);
+});
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dontev from 'dotenv';
-
+import { DataSource } from 'typeorm';
 
 
 const app: Express = express();
@@ -9,12 +9,28 @@ dontev.config();
 // Server port + port fetched from env file
 const port = process.env.PORT;
 
+// Database connection
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    synchronize: true,
+});
+
 
 // Default route
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
-})
+    res.send('Express + TypeScript Server');
+});
 
 
-// Listen to server
-app.listen(port);
+AppDataSource.initialize().then(() => {
+    app.listen(port);
+    console.log('Data Source has benn initialized');
+}).catch((err) => {
+    console.error("Error during Data Source initialization", err);
+});
+
