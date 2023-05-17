8946ebefbec2a69623c5c14a45364a7f096fb35c
JoanaMas <joana.mastianica@gmail.com>
2023-05-17 10:42:50 +0300

    12: Work environment set up: .env, eslint config and .prettierrc files added

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from 'express';
+import dontev from 'dotenv';
 
 
 
 const app: Express = express();
+dontev.config();
 
-// Server port
-const port = 4500;
+// Server port + port fetched from env file
+const port = process.env.PORT;
 
 
 // Default route
